6c534f8b88e22f0901e4054087d68a2d1b2cbfb3
ktmsulaim <ktmsulaim@gmail.com>
2019-06-27 10:07:18 +0530

    users with editing

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,1 +1,62 @@
-<h1>Edit user</h1>
+@extends('layouts.admin')
+
+@section('page-head')
+  Edit user <span class="badge badge-secondary">{{$user->name}}</span>
+@stop
+@section('content')
+  <div class="col-lg-3">
+      <img src="{{$user->photo ? $user->photo->file : '/images/user.png'}}" alt="" class="img-responsive img-rounded">
+      @include('includes.form_error')
+    </div>
+  <div class="col-lg-9">
+    {!! Form::model($user,['method'=>'PATCH', 'action'=>['AdminUsersController@update', $user->id], 'files'=>true]) !!}
+    <div class="row">
+    <div class="form-group col-md-12">
+        {!! Form::label('name', 'Name:', ['class'=>'control-form']) !!}
+        {!! Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Your full name']) !!}
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group col-md-12">
+        {!! Form::label('email', 'Email:', ['class'=>'control-form']) !!}
+        {!! Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'A valid email address']) !!}
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group col-md-12">
+        {!! Form::label('role_id', 'Role:', ['class'=>'control-form']) !!}
+        {!! Form::select('role_id', [''=>'Choose a role'] + $roles, $user->role_id, ['class'=>'form-control']) !!}
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group col-md-12">
+        {!! Form::label('is_active', 'Status:', ['class'=>'control-form']) !!}
+        {!! Form::select('is_active', array(0=>'Not Active', 1=>'Active'), null, ['class'=>'form-control']) !!}
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group col-md-12">
+        {!! Form::label('password', 'Password:', ['class'=>'control-form']) !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group col-md-12">
+        {!! Form::label('photo_id', 'Photo:', ['class'=>'control-form']) !!}
+        {!! Form::file('photo_id', ['class'=>'form-control-file']) !!}
+
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group col-md-12">
+        {!! Form::submit('Update', ['class'=>'btn btn-primary']) !!}
+      </div>
+    </div>
+
+  {!! Form::close() !!}
+  </div>
+  <div class="clearfix"></div>
+
+
+@stop
+
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -10,6 +10,7 @@
     <thead>
       <tr>
         <th>Id</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -24,13 +25,10 @@
     @foreach($users as $user)
       <tr>
         <td>{{$user->id}}</td>
-        <td>{{$user->name}}</td>
+        <td><img src="{{$user->photo ? $user->photo->file : '/images/user.png'}}" alt="{{$user->name}}" height="50" style="border-radius:50%; width:50px"></td>
+      <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
         <td>{{$user->email}}</td>
-        @if($user->role)
-        <td>{{$user->role->name}}</td>
-        @else
-        <td>No role found</td>
-        @endif
+        <td>{{$user->role ? $user->role->name : "User has no role"}}</td>
         <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
         <td>{{$user->created_at->diffForHumans()}}</td>
         <td>{{$user->updated_at->diffForHumans()}}</td>
